824aca2b9a439cfceac080985a91667d9b9e561e
Alex Danilin <Alex39292@mail.ru>
2020-04-18 22:11:33 +0300

    Change propertie name

--- src/test/java/framework/service/PricingCalculatorCreator.java
+++ src/test/java/framework/service/PricingCalculatorCreator.java
@@ -12,7 +12,7 @@ public class PricingCalculatorCreator {
     private static final String TEST_GPU_TYPE = "calculator.GPUType";
     private static final String TEST_LOCAL_SSD = "calculator.localSSD";
     private static final String TEST_DATACENTER_LOCATION = "calculator.datacenterLocation";
-    private static final String TEST_COMMITED_USAGE = "calculator.commitedUsage";
+    private static final String TEST_COMMITTED_USAGE = "calculator.committedUsage";
 
     public static PricingCalculator withProperty() {
         return new PricingCalculator()
@@ -25,6 +25,6 @@ public class PricingCalculatorCreator {
                 .setGPUType(TestDataReader.getTestData(TEST_GPU_TYPE))
                 .setLocalSSD(TestDataReader.getTestData(TEST_LOCAL_SSD))
                 .setDatacenterLocation(TestDataReader.getTestData(TEST_DATACENTER_LOCATION))
-                .setCommittedUsage(TestDataReader.getTestData(TEST_COMMITED_USAGE));
+                .setCommittedUsage(TestDataReader.getTestData(TEST_COMMITTED_USAGE));
     }
 }
